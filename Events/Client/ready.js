677abf6ce929367d7e8c802aa6be65778c4be6ad
Darkeew <darkeewtc@gmail.com>
2022-08-16 00:34:31 +0100

    shortened alot

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -44,78 +44,70 @@ module.exports = {
 
     // Ban Checker
     setInterval(async () => {
-      const bc = await CasesModel.find().sort("-time");
       x = 0;
+      const a = await CasesModel.find();
       do {
-        if (bc[x].type === "ban") {
-          if (bc[x].time) {
-            if (bc[x].expired === false) {
-              break;
-            } else {
-              x++;
-            }
-          } else {
-            x++;
-          }
-        } else {
-          x++;
-        }
-      } while (x !== bc.length);
-      if (!bc[x]) return;
-
-      const user = bc[x].punished;
-      const c = bc[x].case;
-      await CasesModel.findOneAndUpdate({ case: c }, { expired: true });
-      guild.members.unban(user);
+        const c = await CasesModel.findOne({ case: x });
+        if (x > a.length) client.stop = true;
+        if (c.type === "ban") {
+          if (c.expired === false) {
+            if (c.time) {
+              if (c.time < Date.now() / 1000) {
+                await CasesModel.findOneAndUpdate(
+                  { case: x },
+                  { expired: true }
+                );
+                try {
+                  guild.members.unban(c.punished);
+                } catch (e) {
+                  console.log(e);
+                }
+                client.stop = true;
+              } else x++;
+            } else x++;
+          } else x++;
+        } else x++;
+      } while (client.stop !== true);
     }, 5000);
     // Warn Checker
     setInterval(async () => {
-      const wc = await CasesModel.find().sort("-time");
-      x = 0;
+      z = 0;
+      const awc = await CasesModel.find();
+      const wc = await CasesModel.findOne({ case: x });
       do {
-        if (wc[x].type === "warn") {
-          if (wc[x].time) {
-            if (wc[x].expired === false) {
-              break;
-            } else {
-              x++;
-            }
-          } else {
-            x++;
-          }
-        } else {
-          x++;
-        }
-      } while (x !== wc.length);
-      if (!wc[x]) return;
-      const c = wc[x].case;
-      if (wc[x].time < Date.now() / 1000) {
-        const UMM = require("../../Structures/Schema/UserModeration");
-        const rm = await UMM.findOne({ user: wc[x].punished });
-        const nw = rm.warns - 1;
-        await UMM.findOneAndUpdate({ user: wc[x].punished }, { warns: nw });
-        await CasesModel.findOneAndUpdate({ case: c }, { expired: true });
-        const member = guild.members.cache.get(wc[x].punished);
-        member
-          .send({
-            embeds: [
-              new MessageEmbed()
-                .setAuthor({ name: "Case Expired" })
-                .setDescription(
-                  `**Case ${wc[x].case}** has been expired. Be more careful next time and read the rules!
+        if (wc.type === "warn") {
+          if (wc.expired === false) {
+            if (wc.time < Date.now() / 1000) {
+              await CasesModel.findOneAndUpdate({ case: z }, { expired: true });
+              const member = guild.members.cache.get(wc.punished);
+              member
+                .send({
+                  embeds: [
+                    new MessageEmbed()
+                      .setAuthor({ name: "Case Expired" })
+                      .setDescription(
+                        `**Case ${wc[x].case}** has been expired. Be more careful next time and read the rules!
 > Reason of the punishment: \`${wc[x].reason}\`
 > Punisher: <@!${wc[x].punisher}> (${wc[x].punisher})
 
 > Reason for expire: \`14 days have passed since case ${wc[x].case} was made\`.
                 `
-                )
-                .setColor("DARK_GOLD"),
-            ],
-          })
-          .catch((error) => {
-            // get ignored
-          });
-      }
+                      )
+                      .setColor("DARK_GOLD"),
+                  ],
+                })
+                .catch((error) => {
+                  // get ignored
+                });
+              const UMM = require("../../Structures/Schema/UserModeration");
+              const rm = await UMM.findOne({ user: wc.punished });
+              const nw = rm.warns - 1;
+              await UMM.findOneAndUpdate({ user: wc.punished }, { warns: nw });
+              client.stop2 = true;
+            } else z++;
+          } else z++;
+        } else z++;
+      } while (client.stop2 !== true);
     }, 5000);
     setInterval(async () => {
       const verification = await RestartsModel.findOne();
