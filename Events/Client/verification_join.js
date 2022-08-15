31b6a756e18bc4b546dbaa23a947b3e8664c00e0
Darkeew <darkeewtc@gmail.com>
2022-08-15 23:42:26 +0100

    verification

---
+++ Events/Client/verification_join.js
@@ -0,0 +1,29 @@
+const { Client } = require("discord.js");
+
+module.exports = {
+  name: "guildMemberAdd",
+  /**
+   * @param {Client} client
+   * @param {Guild} guild
+   */
+  async execute(member, client) {
+    const RestartsModel = require("../../Structures/Schema/Restarts");
+    const status = await RestartsModel.findOne();
+    if (status.verification === false) {
+      //Change this
+      //const g = "986357448925401168"; //Test server id
+      const g = "946518364216520774"; //Puppet server id
+      //const verified = "993169053906636890"; //Test server id
+      const verified = "993169053906636890"; //Puppet server id
+
+      const guild = client.guilds.cache.get(g);
+
+      const member = await guild.members.fetch(member.id);
+      try {
+        member.roles.add(verified);
+      } catch (e) {
+        //ignored
+      }
+    }
+  },
+};
