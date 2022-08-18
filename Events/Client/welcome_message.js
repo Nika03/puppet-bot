74c9194b2b7f1a6da23e5beec525e3dd3ead9bf3
Darkeew <darkeewtc@gmail.com>
2022-08-18 22:58:59 +0100

    .

--- Events/Client/welcome_message.js
+++ Events/Client/welcome_message.js
@@ -1,21 +1,40 @@
-const { Client } = require("discord.js");
+const { Client, MessageEmbed } = require("discord.js");
 
 module.exports = {
   name: "guildMemberUpdate",
   /**
    * @param {Client} client
-   * @param {Guild} guild
    */
   async execute(newMember, oldMember, client) {
-    if (oldMember._roles.includes("970229987405877259")) {
-      if (!newMember._roles.includes("970229987405877259")) {
+    if (oldMember._roles.includes("946524059724820500")) {
+      if (!newMember._roles.includes("946524059724820500")) {
         const c = "946520764297912343";
         const g = "946518364216520774";
 
         const guild = client.guilds.cache.get(g);
         const channel = guild.channels.cache.get(c);
 
-        channel.send(`${oldMember} has been changed`);
+        const r = Math.floor(Math.random() * 7);
+        if (r === "1") {
+          client.message = `${oldMember} is new here!`;
+        } else if (r === "2") {
+          client.message = `Oh look, ${oldMember} is finally here.`;
+        } else if (r === "3") {
+          client.message = `A very warm welcome to ${oldMember}`;
+        } else if (r === "4") {
+          client.message = `We have all gathered to see you, ${oldMember}.`;
+        } else if (r === "5") {
+          client.message = `${oldMember} came here very suddenly.`;
+        } else if (r === "6") {
+          client.message = `${oldMember} has traveled for a long time to find us.`;
+        } else if (r === "7") {
+          client.message = `${oldMember} has arrived at last.`;
+        } else {
+          client.message = `This is just the beginning for ${oldMember}`;
+        }
+        channel.send({
+          embeds: [new MessageEmbed().setDescription(`${client.message}`)],
+        });
       }
     }
   },
