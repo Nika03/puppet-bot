bf9ce4c71df66ba2e484046a429f5b7685a921b8
Darkeew <darkeewtc@gmail.com>
2022-08-18 22:46:00 +0100

    .

--- Events/Client/welcome_message.js
+++ Events/Client/welcome_message.js
@@ -7,6 +7,16 @@ module.exports = {
    * @param {Guild} guild
    */
   async execute(newMember, oldMember, client) {
-    console.log(newMember._roles, oldMember._roles);
+    if (!oldMember._roles.includes("970229987405877259")) {
+      if (newMember._roles.includes("970229987405877259")) {
+        const c = "946520764297912343";
+        const g = "946518364216520774";
+
+        const guild = client.guilds.cache.get(g);
+        const channel = guild.channels.cache.get(c);
+
+        channel.send(`${oldMember} has been changed`);
+      }
+    }
   },
 };
