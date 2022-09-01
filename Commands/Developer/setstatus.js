92480ac0e5a9502bef840dba87d9e9a6254f96d9
Darkeew <83980195+Darkeew@users.noreply.github.com>
2022-09-01 13:05:38 +0100

    new command

---
+++ Commands/Developer/setstatus.js
@@ -0,0 +1,63 @@
+const { CommandInteraction, MessageEmbed, Message } = require("discord.js");
+
+module.exports = {
+  name: "setstatus",
+  description: "Change the bot's status.",
+  permission: "ADMINISTRATOR",
+  options: [
+    {
+      name: `type`,
+      description: `The type of the status`,
+      required: true,
+      type: `STRING`,
+      choices: [
+        {
+          name: "Listening",
+          value: "LISTENING",
+        },
+        {
+          name: "Playing",
+          value: "PLAYING",
+        },
+        {
+          name: "Streaming",
+          value: "STREAMING",
+        },
+        {
+          name: "Watching",
+          value: "WATCHING",
+        },
+      ],
+    },
+    {
+      name: `status`,
+      description: `The status for the bot.`,
+      required: true,
+      type: `STRING`,
+    },
+  ],
+  /**
+   * @param {CommandInteraction} interaction
+   * @param {Client} client
+   */
+  async execute(interaction, client) {
+    const statustype = await interaction.options.getString("type");
+    const status = await interaction.options.getString("status");
+
+    try {
+      client.user.setActivity(status, { type: statustype });
+    } catch (e) {
+      console.log(e);
+      client.acterr = true;
+      return interaction.reply(
+        `There was an error setting the bot's status, ${e.toString()}`
+      );
+    }
+    if (!client.acterr) {
+      interaction.reply({
+        content: "Successfuly changed the bot's status.",
+        ephemeral: true,
+      });
+    }
+  },
+};
