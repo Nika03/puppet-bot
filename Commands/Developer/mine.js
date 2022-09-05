22a9cbd61773bafb39da898bef54b57698df8a71
Darkeew <83980195+Darkeew@users.noreply.github.com>
2022-09-05 19:51:53 +0100

    command

---
+++ Commands/Developer/mine.js
@@ -0,0 +1,17 @@
+const { CommandInteraction, MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "mine",
+  description: "Mine some items.",
+  permission: "SEND_MESSAGES",
+  /**
+   * @param {CommandInteraction} interaction
+   * @param {Client} client
+   */
+  async execute(interaction, client) {
+    interaction.reply(
+      "I am not adding this command anytime soon. - <@!452436342841016341> at <t:1662403856>"
+    );
+    console.log(Date.now() / 1000);
+  },
+};
