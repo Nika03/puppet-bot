904bc80cf99401d6ea63fd375b90efbe4f2cbee8
Darkeew <83980195+Darkeew@users.noreply.github.com>
2022-09-05 15:22:14 +0100

    fixed

--- Events/Client/messageChecker.js
+++ Events/Client/messageChecker.js
@@ -23,6 +23,7 @@ module.exports = {
       if (message.toString().includes(filter[x])) {
         const guild = client.guilds.cache.get("946518364216520774");
         const channel = client.channels.cache.get(message.channel.id);
+        x = filter.length;
         console.log("wah");
         channel.send("wah");
         const member = guild.members.cache.get(message.author.id);
