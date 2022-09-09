0307098b35a8b3f10759468a1cc9b835868e72a2
Darkeew <83980195+Darkeew@users.noreply.github.com>
2022-09-10 00:06:46 +0100

    added no time

--- Commands/Developer/clickthebutton.js
+++ Commands/Developer/clickthebutton.js
@@ -9,11 +9,20 @@ module.exports = {
   name: "clickthebutton",
   description: "Force the button event in general.",
   permission: "MANAGE_MESSAGES",
+  options: [
+    {
+      name: "no-time",
+      description: "No time.",
+      type: "BOOLEAN",
+      required: false,
+    },
+  ],
   /**
    * @param {CommandInteraction} interaction
    * @param {Client} client
    */
   async execute(interaction, client) {
+    const notime = interaction.options.getBoolean("no-time");
     client.buttonclicked = false;
     client.buttontimer = Date.now() / 1000;
     interaction.reply({
@@ -32,26 +41,28 @@ module.exports = {
         ),
       ],
     });
-    setTimeout(() => {
-      if (client.buttonclicked === false) {
-        interaction.editReply({
-          embeds: [
-            new MessageEmbed()
-              .setAuthor({ name: "Click the Button" })
-              .setDescription("Nobody clicked the button, shame.")
-              .setColor("DARK_NAVY"),
-          ],
-          components: [
-            new MessageActionRow().addComponents(
-              new MessageButton()
-                .setCustomId(`event_button`)
-                .setLabel(`button`)
-                .setStyle(`SUCCESS`)
-                .setDisabled(true)
-            ),
-          ],
-        });
-      }
-    }, 15000);
+    if (!notime) {
+      setTimeout(() => {
+        if (client.buttonclicked === false) {
+          interaction.editReply({
+            embeds: [
+              new MessageEmbed()
+                .setAuthor({ name: "Click the Button" })
+                .setDescription("Nobody clicked the button, shame.")
+                .setColor("DARK_NAVY"),
+            ],
+            components: [
+              new MessageActionRow().addComponents(
+                new MessageButton()
+                  .setCustomId(`event_button`)
+                  .setLabel(`button`)
+                  .setStyle(`SUCCESS`)
+                  .setDisabled(true)
+              ),
+            ],
+          });
+        }
+      }, 15000);
+    }
   },
 };
