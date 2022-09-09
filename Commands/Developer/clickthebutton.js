8fb458e0cb306ed2001ed13e8004ef62d4ff45c5
Darkeew <83980195+Darkeew@users.noreply.github.com>
2022-09-09 20:36:09 +0100

    fixed

--- Commands/Developer/clickthebutton.js
+++ Commands/Developer/clickthebutton.js
@@ -16,47 +16,42 @@ module.exports = {
   async execute(interaction, client) {
     client.buttonclicked = false;
     client.buttontimer = Date.now() / 1000;
-    interaction
-      .reply({
-        embeds: [
-          new MessageEmbed()
-            .setAuthor({ name: "Click the Button" })
-            .setDescription(
-              "The event is simple, click the button. No rewards."
-            )
-            .setColor("DARK_NAVY"),
-        ],
-        //components: [
-        //  new MessageActionRow().addComponents(
-        //    new MessageButton()
-        //      .setCustomId(`event_button`)
-        //      .setLabel(`button`)
-        //      .setStyle(`SUCCESS`)
-        //  ),
-        //],
-      })
-      .then((message) => {
-        setTimeout(() => {
-          if (client.buttonclicked === false) {
-            message.edit({
-              embeds: [
-                new MessageEmbed()
-                  .setAuthor({ name: "Click the Button" })
-                  .setDescription("Nobody clicked the button, shame.")
-                  .setColor("DARK_NAVY"),
-              ],
-              components: [
-                new MessageActionRow().addComponents(
-                  new MessageButton()
-                    .setCustomId(`event_button`)
-                    .setLabel(`button`)
-                    .setStyle(`SUCCESS`)
-                    .setDisabled(true)
-                ),
-              ],
-            });
-          }
-        }, 15000);
-      });
+    interaction.reply({
+      embeds: [
+        new MessageEmbed()
+          .setAuthor({ name: "Click the Button" })
+          .setDescription("The event is simple, click the button. No rewards.")
+          .setColor("DARK_NAVY"),
+      ],
+      //components: [
+      //  new MessageActionRow().addComponents(
+      //    new MessageButton()
+      //      .setCustomId(`event_button`)
+      //      .setLabel(`button`)
+      //      .setStyle(`SUCCESS`)
+      //  ),
+      //],
+    });
+    setTimeout(() => {
+      if (client.buttonclicked === false) {
+        interaction.editReply({
+          embeds: [
+            new MessageEmbed()
+              .setAuthor({ name: "Click the Button" })
+              .setDescription("Nobody clicked the button, shame.")
+              .setColor("DARK_NAVY"),
+          ],
+          components: [
+            new MessageActionRow().addComponents(
+              new MessageButton()
+                .setCustomId(`event_button`)
+                .setLabel(`button`)
+                .setStyle(`SUCCESS`)
+                .setDisabled(true)
+            ),
+          ],
+        });
+      }
+    }, 15000);
   },
 };
