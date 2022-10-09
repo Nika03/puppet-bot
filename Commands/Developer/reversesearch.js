1b9e23797ddc4a1c2cbf262d35a10e577e241b1c
Darkeew <83980195+Darkeew@users.noreply.github.com>
2022-10-09 15:52:16 +0100

    added

---
+++ Commands/Developer/reversesearch.js
@@ -0,0 +1,83 @@
+const { CommandInteraction, MessageEmbed } = require("discord.js");
+const axios = require("axios");
+const { reverseimageapikey } = require("../../Structures/config.json");
+
+module.exports = {
+  name: "reverse-search",
+  description: "Reverse search an image.",
+  permission: "ADMINISTRATOR",
+  options: [
+    {
+      name: "url",
+      description: "The URL of the image.",
+      type: "STRING",
+      required: true,
+    },
+  ],
+  /**
+   * @param {CommandInteraction} interaction
+   * @param {Client} client
+   */
+  async execute(interaction, client) {
+    const url = interaction.options.getString("url");
+    interaction.deferReply();
+    function isValidURL(url) {
+      try {
+        return Boolean(new URL(url));
+      } catch (e) {
+        return false;
+      }
+    }
+    if (isValidURL(url) === false) {
+      return interaction.editReply({
+        content: `\`${url}\` is not a valid url.`,
+        ephemeral: true,
+      });
+    }
+
+    const options = {
+      method: "GET",
+      url: "https://reverse-image-search-by-copyseeker.p.rapidapi.com/",
+      params: { imageUrl: `${url}` },
+      headers: {
+        "X-RapidAPI-Key": reverseimageapikey,
+        "X-RapidAPI-Host": "reverse-image-search-by-copyseeker.p.rapidapi.com",
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        const match = response.data.Pages[1];
+        if (!match)
+          return interaction.editReply({ content: "No matches found." });
+        const image = match.MatchingImages[1];
+        interaction.editReply({
+          embeds: [
+            new MessageEmbed()
+              .setAuthor({
+                name: `${response.data.Pages.length} matches found`,
+              })
+              .setDescription(
+                `
+Image found:
+
+Origin: ${match.Url}
+Matching Percentage: \`${match.Rank * 10} / 100\`
+            `
+              )
+              .setImage(image)
+              .setFooter({ text: `Requested by ${interaction.user.tag}` })
+              .setTimestamp()
+              .setColor("BLURPLE"),
+          ],
+        });
+      })
+      .catch(function (error) {
+        console.error(error);
+        return interaction.editReply({
+          content: "Something went wrong. (The link may not be valid.)",
+        });
+      });
+  },
+};
