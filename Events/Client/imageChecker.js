54a92121e9b825acaa8d74cb81e082f49f74c460
Darkeew <83980195+Darkeew@users.noreply.github.com>
2022-10-10 20:43:54 +0100

    fixed

--- Events/Client/imageChecker.js
+++ Events/Client/imageChecker.js
@@ -11,28 +11,32 @@ module.exports = {
   async execute(message, client) {
     const guild = client.guilds.cache.get(message.guild.id);
     if (message.toString().startsWith("https")) {
-      try {
-        axios
-          .get("https://api.moderatecontent.com/moderate/", {
-            params: {
-              key: moderatecontentkey,
-              url: message.toString(),
-            },
-          })
-          .then((response) => {
+      axios
+        .get("https://api.moderatecontent.com/moderate/", {
+          params: {
+            key: moderatecontentkey,
+            url: message.toString(),
+          },
+        })
+        .then((response) => {
+          try {
             if (response.data.predictions.adult > 75) {
               const channel = guild.channels.cache.get(message.channel.id);
-              message.delete();
-              channel.send({
-                content: `${message.author}`,
-                embeds: [
-                  new MessageEmbed()
-                    .setDescription(
-                      "Your message has been deleted due to containing sensitive content."
-                    )
-                    .setColor("BLURPLE"),
-                ],
-              });
+              try {
+                message.delete();
+                channel.send({
+                  content: `${message.author}`,
+                  embeds: [
+                    new MessageEmbed()
+                      .setDescription(
+                        "Your message has been deleted due to containing sensitive content."
+                      )
+                      .setColor("BLURPLE"),
+                  ],
+                });
+              } catch (e) {
+                console.log(e.toString());
+              }
             }
             const logs = guild.channels.cache.get("1028748150149763092");
             logs.send({
@@ -51,10 +55,16 @@ Teen rating: \`${response.data.predictions.teen}\`
                   .setImage(`${message.toString()}`),
               ],
             });
-          });
-      } catch (e) {
-        console.log(e);
-      }
+          } catch (e) {
+            if (
+              !e.toString(
+                "TypeError: Cannot read properties of undefined (reading 'adult')"
+              )
+            ) {
+              console.log(e);
+            }
+          }
+        });
     } else if (message.attachments.size < 1) {
       return;
     } else {
@@ -71,17 +81,21 @@ Teen rating: \`${response.data.predictions.teen}\`
           .then((response) => {
             if (response.data.predictions.adult > 75) {
               const channel = guild.channels.cache.get(message.channel.id);
-              message.delete();
-              channel.send({
-                content: `${message.author}`,
-                embeds: [
-                  new MessageEmbed()
-                    .setDescription(
-                      "Your message has been deleted due to containing sensitive content."
-                    )
-                    .setColor("BLURPLE"),
-                ],
-              });
+              try {
+                message.delete();
+                channel.send({
+                  content: `${message.author}`,
+                  embeds: [
+                    new MessageEmbed()
+                      .setDescription(
+                        "Your message has been deleted due to containing sensitive content."
+                      )
+                      .setColor("BLURPLE"),
+                  ],
+                });
+              } catch (e) {
+                console.log(e.toString());
+              }
             }
             const logs = guild.channels.cache.get("1028748150149763092");
             logs.send({
