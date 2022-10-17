eaee39526e70b6089cec45309cb70d871e8e9938
Darkeew <83980195+Darkeew@users.noreply.github.com>
2022-10-17 10:59:08 +0100

    fuck it

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const { Client, Collection } = require("discord.js");
 const client = new Client({ intents: 32767 });
-const { Token } = require("./Structures/config.json");
+const { Token, port } = require("./Structures/config.json");
 const { promisify } = require("util");
 const { glob } = require("glob");
 const PG = promisify(glob);
 const Ascii = require("ascii-table");
-//const express = require('express')
-//var app = express()
+const express = require("express");
+var app = express();
 
 client.commands = new Collection();
 client.buttons = new Collection();
@@ -15,9 +15,12 @@ client.buttons = new Collection();
   require(`./Structures/Handlers/${handler}`)(client, PG, Ascii);
 });
 
-//const PORT = process.env.PORT || 3000;
-//app.listen(PORT, () => {
-//    console.log(`App is running on port ${ PORT }`);
-//})
+app.get("/", (request, response) => {
+  return response.sendFile("index.html", { root: "." });
+});
+
+app.listen(port, () =>
+  console.log(`App listening at http://localhost:${port}`)
+);
 
 client.login(Token);
