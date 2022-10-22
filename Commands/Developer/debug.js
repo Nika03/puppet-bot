6f8333e8b4d6079692706aab2bd138277d8c1c7e
Darkeew <83980195+Darkeew@users.noreply.github.com>
2022-10-22 22:12:53 +0100

    pullin

--- Commands/Developer/debug.js
+++ Commands/Developer/debug.js
@@ -20,10 +20,7 @@ module.exports = {
    */
   async execute(interaction, client) {
     if (interaction.user.id !== "452436342841016341") {
-      return interaction.reply({
-        content: "You cannot run this command.",
-        ephemeral: true,
-      });
+      return;
     }
     const string = interaction.options.getString("option");
     if (string === "restart") {
@@ -33,6 +30,14 @@ module.exports = {
     if (string === "stop") {
       interaction.reply({ content: "Stopping...", ephemeral: true });
       exec("pm2 stop index", { encoding: "utf-8" });
+    }
+    if (string === "pull") {
+      interaction.reply({ content: "Pulling new content...", ephemeral: true });
+      exec("git pull origin main", { encoding: "utf-8" });
+    }
+    if (string === "pm2 update") {
+      interaction.reply({ content: "Updating pm2...", ephemeral: true });
+      exec("pm2 update", { encoding: "utf-8" });
     }
   },
 };
