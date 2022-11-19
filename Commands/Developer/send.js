22486f8d28bac02558f808c5e2b61ef89cf5480b
Darkeew <83980195+Darkeew@users.noreply.github.com>
2022-11-19 21:32:44 +0000

    added

--- Commands/Developer/send.js
+++ Commands/Developer/send.js
@@ -4,6 +4,8 @@ const {
   MessageActionRow,
   MessageButton,
 } = require("discord.js");
+const User = require("../../Structures/Schema/User");
+
 module.exports = {
   name: "send",
   description: "Send a programmed message to a certain channel.",
@@ -16,37 +18,42 @@ module.exports = {
    */
   async execute(interaction, client) {
     if (interaction.user.id !== "452436342841016341") return;
+    console.log("test");
+    var find = await User.find().sort({ button_presses: -1 });
+    const arrayUser = [];
+    const arrayCount = [];
+    var counter = 0;
+    while (counter !== 10) {
+      console.log(counter);
+      arrayUser.push(find[counter].user);
+      arrayCount.push(find[counter].button_presses);
+      counter++;
+    }
     const guild = client.guilds.cache.get("946518364216520774");
     const channel = guild.channels.cache.get(interaction.channel.id);
     channel.send({
       embeds: [
         new MessageEmbed()
-          .setTitle("Press The Button!")
+          .setTitle("Clicks Leaderboard")
           .setDescription(
             `
-      Its simple, press the button, and the counter will go up!
+**These are the top 10 people that have clicked the button!**
 
-      > __Counter:__ \`0 clicks\`
-      
-      Current Main Goal: \`150'000 clicks\`
-      > Prize: \`Nitro Regular\`
+> Top 1: <@!${arrayUser[0]}> with **${arrayCount[0]}** clicks!
+> Top 2: <@!${arrayUser[1]}> with **${arrayCount[1]}** clicks!
+> Top 3: <@!${arrayUser[2]}> with **${arrayCount[2]}** clicks!
+> Top 4: <@!${arrayUser[3]}> with **${arrayCount[3]}** clicks!
+> Top 5: <@!${arrayUser[4]}> with **${arrayCount[4]}** clicks!
+> Top 6: <@!${arrayUser[5]}> with **${arrayCount[5]}** clicks!
+> Top 7: <@!${arrayUser[6]}> with **${arrayCount[6]}** clicks!
+> Top 8: <@!${arrayUser[7]}> with **${arrayCount[7]}** clicks!
+> Top 9: <@!${arrayUser[8]}> with **${arrayCount[8]}** clicks!
+> Top 10: <@!${arrayUser[9]}> with **${arrayCount[9]}** clicks!
 
-      Current Subgoal: \`5'000 clicks\`
-      > Prize: \`Top 10 will get a custom role giveaway.\`
-
-      Last Updated at: <t:${Math.floor(Date.now() / 1000)}:R>
-      *If the button isnt working, please DM <@!452436342841016341>. The counter will update each minute.*
+Updated at: <t:${Math.floor(Date.now() / 1000)}>
       `
           )
-          .setColor("DARK_RED"),
-      ],
-      components: [
-        new MessageActionRow().addComponents(
-          new MessageButton()
-            .setCustomId("press-the-button")
-            .setLabel("Click Me!")
-            .setStyle("DANGER")
-        ),
+          .setColor("BLURPLE"),
       ],
     });
   },
