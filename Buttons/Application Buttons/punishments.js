d34b5f40dcb9c8f60d5fc0f196ce922684b11f5b
Darkeew <83980195+Darkeew@users.noreply.github.com>
2023-01-14 16:55:46 +0000

    folders

---
+++ Buttons/Application Buttons/punishments.js
@@ -0,0 +1,43 @@
+const {
+  Modal,
+  TextInputComponent,
+  MessageActionRow,
+  MessageEmbed,
+} = require("discord.js");
+const CasesModel = require("../../Structures/Schema/Cases");
+
+module.exports = {
+  id: "punishments",
+  permission: "SEND_MESSAGES",
+  async execute(interaction, client) {
+    const users = await CasesModel.find();
+    var counter = 0;
+    users.forEach((c) => {
+      if (
+        c.punished == interaction.user.id &&
+        c.type == "warn" &&
+        c.expired == false
+      ) {
+        counter++;
+      }
+    });
+    if (counter == 0) {
+      var description = `You have **0** active warnings.`;
+    } else if (counter == 1) {
+      var description = `You have **1** active warning.`;
+    } else {
+      var description = `You have **${counter}** active warnings.`;
+    }
+    interaction.reply({
+      embeds: [
+        new MessageEmbed()
+          .setDescription(description)
+          .setAuthor({ name: "Active Warnings" })
+          .setColor("DARK_RED")
+          .setTimestamp()
+          .setFooter({ text: "Requested at" }),
+      ],
+      ephemeral: true,
+    });
+  },
+};
